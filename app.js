33e8fbf816d52a8a33b20ad97b7dd73e96630f94
Gonzalo Arró <gonzalo.arro@hotmail.com>
2018-05-03 16:51:27 -0300

    Router para el admin.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 app.use(function (req, res, next) {
   res.status(404).send("La página solicitada no existe.")
