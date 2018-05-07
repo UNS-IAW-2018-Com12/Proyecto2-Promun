aa6813a36930d8e38a778e7117c7963f412eb89c
DESKTOP-THQ3JFB\joaco <joacolabrisca>
2018-05-07 18:57:58 -0300

    make it better

--- app_server/controllers/userPageController.js
+++ app_server/controllers/userPageController.js
@@ -3,6 +3,7 @@ var Grupo = mongoose.model('Grupo');
 var Usuario = mongoose.model('Usuario');
 var PartidosFaseFinal = mongoose.model('PartidosFaseFinal');
 
+/*
 var isAuthenticated = function (req, res, next) {
 	// if user is authenticated in the session, call the next() to call the next request handler
 	// Passport adds this method to request object. A middleware is allowed to add properties to
@@ -12,6 +13,7 @@ var isAuthenticated = function (req, res, next) {
 	// if the user is not authenticated then redirect him to the login page
 	res.redirect('/');
 }
+*/
 
 var userPage = (req, res) => {
 	if(req.isAuthenticated()) {
