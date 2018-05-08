2b4dfc525c19b14dfc74c1b7d05e23908838f268
Toshiba\Scarface <joacolabrisca@gmail.com>
2018-05-08 16:52:08 -0300

    user

--- app_server/controllers/userPageController.js
+++ app_server/controllers/userPageController.js
@@ -3,20 +3,9 @@ var Grupo = mongoose.model('Grupo');
 var Usuario = mongoose.model('Usuario');
 var PartidosFaseFinal = mongoose.model('PartidosFaseFinal');
 
-/*
-var isAuthenticated = function (req, res, next) {
-	// if user is authenticated in the session, call the next() to call the next request handler
-	// Passport adds this method to request object. A middleware is allowed to add properties to
-	// request and response objects
-	if (req.isAuthenticated())
-		return next();
-	// if the user is not authenticated then redirect him to the login page
-	res.redirect('/');
-}
-*/
-
 var userPage = (req, res) => {
 	if(req.isAuthenticated()) {
+		console.log(req.user);
 	  Grupo.find().sort({letra: 'asc'}).then((grupos) => {
 	    Usuario.find({tipo: 'user'}, {username: 1, puntaje: 1}).sort({puntaje: -1}).then((usuarios) => {
 	      PartidosFaseFinal.find().sort({nro_partido: 'asc'}).then((partidos) => {
