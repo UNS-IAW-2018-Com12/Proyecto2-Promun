2b178f01fb6bd9ac15127a5b05e1f24ef83a3427
DESKTOP-THQ3JFB\joaco <joacolabrisca>
2018-05-09 02:19:46 -0300

    ahora

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('./app_server/models/db');
 
 var app = express();
 
-// view engine setup
+// Configuring view engine
 app.set('views', path.join(__dirname, 'app_server/views'));
 app.set('view engine', 'hbs');
 const hbs = require('hbs');
@@ -21,20 +20,18 @@ hbs.registerHelper(helpers);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Configuring Sessions and Cookies
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+app.use(cookieParser());
+app.use(expressSession({secret: "Es un secreto"}));
 
 // Configuring Passport
 var passport = require('passport');
-var expressSession = require('express-session');
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Using the flash middleware provided by connect-flash to store messages in session
-// and displaying in templates
-var expressSession = require('express-session');
-app.use(expressSession({secret: "Es un secreto"}));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Confuring Connecting-flash
 var flash = require('connect-flash');
@@ -44,7 +41,7 @@ app.use(flash());
 var initPassport = require('./app_server/passport/init');
 initPassport(passport);
 
-// Configure routes
+// Configure routers
 var indexRouter = require('./app_server/routes/index')(passport);
 var usersRouter = require('./app_server/routes/users');
 var adminRouter = require('./app_server/routes/admin');
@@ -66,7 +63,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Configure error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
