d5486ab3712c4705abc51ea9c589ebda5b781438
joaco <joacolabrisca@gmail.com>
2018-06-12 15:58:01 -0300

    a heroku.

--- app_server/routes/promun-api.js
+++ app_server/routes/promun-api.js
@@ -3,30 +3,12 @@ var router = express.Router();
 
 var ctrlAPI = require('../controllers/apiController');
 
-//Funcion para proteccion de rutas
-function authenticate(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    } else {
-        res.redirect('/');
-    }
-}
+router.post('/agregarPronostico', ctrlAPI.addPronostico);
 
-//Rutas de la aplicacion
-router.post('/agregarPronostico', authenticate, (req, res, next) => {
-    ctrlAPI.addPronostico;
-});
+router.post('/borrarPronostico', ctrlAPI.deletePronostico);
 
-router.post('/borrarPronostico', authenticate, (req, res, next) => {
-    ctrlAPI.deletePronostico;
-});
+router.post('/agregarPronosticoFaseFinal', ctrlAPI.addPronosticoFaseFinal);
 
-router.post('/agregarPronosticoFaseFinal', authenticate, (req, res, next) => {
-    ctrlAPI.addPronosticoFaseFinal;
-});
-
-router.get('/descripcion/:equipo', authenticate, (req, res, next) => {
-    ctrlAPI.getDescripcionEquipo;
-});
+router.get('/descripcion/:equipo', ctrlAPI.getDescripcionEquipo);
 
 module.exports = router;
