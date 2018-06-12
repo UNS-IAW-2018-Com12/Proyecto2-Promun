a0cf793324a57131801a791167204b98318d6676
Toshiba\Scarface <joacolabrisca@gmail.com>
2018-06-12 17:59:17 -0300

    make it better

--- app_server/controllers/userPageController.js
+++ app_server/controllers/userPageController.js
@@ -6,12 +6,12 @@ var PartidoFaseFinal = mongoose.model('PartidoFaseFinal');
 var userPage = (req, res) => {
     console.log("BIENVENIDO!!" + req.username);
     Grupo.find().sort({letra: 'asc'}).then((grupos) => {
-      Usuario.find({tipo: 'user'}, {username: 1, puntaje: 1}).sort({puntaje: -1}).then((usuarios) => {
+      Usuario.find({ 'id' : profile.id}).sort({puntaje: -1}).then((usuario) => {
         PartidoFaseFinal.find().sort({nro_partido: 'asc'}).then((partidos) => {
           res.render('user', {
             title: 'Promun',
             grupos: grupos,
-            usuarios: usuarios,
+            usuario: usuario,
             partidosFaseFinal: partidos
           });
         });
